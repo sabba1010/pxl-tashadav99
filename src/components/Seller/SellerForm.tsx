b50557ea5b3f0d018d001a82143ee9055a2bb607
Sabba Hossain <sabbahossain@gmail.com>
2025-12-15 16:02:07 +0600

    seller

---
+++ src/components/Seller/SellerForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SellerForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SellerForm;
---
+++ src/components/Seller/SellerPay.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SellerPay = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SellerPay;
