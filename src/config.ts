818c629cadf0a808df65f84f1fbcb1789bef34f4
Sabba Hossain <sabbahossain@gmail.com>
2026-03-10 09:37:23 +0600

    add

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@
 const isDevelopment = process.env.NODE_ENV === "development";
 
 export const API_BASE_URL = isDevelopment
-    ? "http://72.244.153.24:3200"
-    : "http://72.244.153.24:3200";
+  ? "http://72.244.153.24:3200"
+  : "https://acctempire.com/api";
 
 export const FLUTTERWAVE_BACKEND_URL = `${API_BASE_URL}/flutterwave`;
 export const KORAPAY_BACKEND_URL = `${API_BASE_URL}/korapay`;
